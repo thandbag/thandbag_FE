5fc8787f0b8d6a7fc65bee75f318066b07789a09
가즈아 <wjsdydxo45@naver.com>
2021-12-22 16:05:03 +0900

    first

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import styled from 'styled-components';
-import './App.css';
-import {Route} from "react-router-dom";
+import React from "react";
+import styled from "styled-components";
+import "./App.css";
+import { Route } from "react-router-dom";
 import Main from "../pages/Main";
-import Login from '../pages/Login';
+import Login from "../pages/Login";
 import Join from "../pages/Join";
 import TbSelect from "../pages/TbSelect";
 import TbWrite from "../pages/TbWrite";
@@ -15,37 +15,34 @@ import TbChatList from "../pages/TbChatList";
 import TbChatDetail from "../pages/TbChatDetail";
 import MyPage from "../pages/MyPage";
 import MyEdit from "../pages/MyEdit";
-import { ConnectedRouter } from 'connected-react-router'
-import { history } from '../redux/configureStore'
-
+import { ConnectedRouter } from "connected-react-router";
+import { history } from "../redux/configureStore";
 
 function App() {
   return (
     <>
-    <Container>
-      <div className='wrap'>
-        <ConnectedRouter history={history}>
-          <Route path={"/"} exact component={Main}/>
-          <Route path={"/login"} exact component={Login}/>
-          <Route path={"/join"} exact component={Join}/>
-          <Route path={"/TbSelect"} exact component={TbSelect}/>
-          <Route path={"/TbWrite"} exact component={TbWrite}/>
-          <Route path={"/TbList"} exact component={TbList}/>
-          <Route path={"/TbOneDetail"} exact component={TbOneDetail}/>
-          <Route path={"/TbTwoDetail"} exact component={TbTwoDetail}/>
-          <Route path={"/TbNotice"} exact component={TbNotice}/>
-          <Route path={"/TbChatList"} exact component={TbChatList}/>
-          <Route path={"/TbChatDetail"} exact component={TbChatDetail}/>
-          <Route path={"/MyPage"} exact component={MyPage}/>
-          <Route path={"/MyEdit"} exact component={MyEdit}/>
-        </ConnectedRouter>
-      </div>
-    </Container>
-    
-    
+      <Container>
+        <div className="wrap">
+          <ConnectedRouter history={history}>
+            <Route path={"/"} exact component={Main} />
+            <Route path={"/login"} exact component={Login} />
+            <Route path={"/join"} exact component={Join} />
+            <Route path={"/TbSelect"} exact component={TbSelect} />
+            <Route path={"/TbWrite"} exact component={TbWrite} />
+            <Route path={"/TbList"} exact component={TbList} />
+            <Route path={"/TbOneDetail"} exact component={TbOneDetail} />
+            <Route path={"/TbTwoDetail"} exact component={TbTwoDetail} />
+            <Route path={"/TbNotice"} exact component={TbNotice} />
+            <Route path={"/TbChatList"} exact component={TbChatList} />
+            <Route path={"/TbChatDetail"} exact component={TbChatDetail} />
+            <Route path={"/MyPage"} exact component={MyPage} />
+            <Route path={"/MyEdit"} exact component={MyEdit} />
+          </ConnectedRouter>
+        </div>
+      </Container>
     </>
   );
-};
+}
 
 const Container = styled.div`
   display: flex;
@@ -53,10 +50,10 @@ const Container = styled.div`
   justify-content: flex-start;
   align-items: center;
   height: 100%;
-  background-color: black;
+  background-color: #e5e5e5;
   overflow: hidden;
+
   .wrap {
-    
     width: 100%;
     height: 100%;
     max-width: 420px;
@@ -64,17 +61,16 @@ const Container = styled.div`
     margin: 0 auto;
     padding: 0 auto;
     box-sizing: border-box;
-    background-color: gray;
+    background-color: #efefef;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
 
-    @media screen and (min-width: 1024px){
-    position: relative;
-    left:11%;
-    top:0%;
-    overflow: auto;
+    @media screen and (min-width: 1024px) {
+      position: relative;
+      left: 11%;
+      top: 0%;
+      overflow: auto;
+    }
   }
-  }
-  
-
 `;
 
 export default App;
